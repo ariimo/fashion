08f025bb5b1691213ca9f93584a252c7130218a5
ariimo <159447466+ariimo@users.noreply.github.com>
2026-03-08 01:35:11 +0900

    feat: setting backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SearchModule } from './search/search.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    ServeStaticModule.forRoot({
+      // 부모 폴더의 images/val2020 경로를 잡습니다.
+      rootPath: join(process.cwd(), '../..', 'images', 'test'),
+      serveRoot: '/static-images',
+    }),
+    SearchModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
